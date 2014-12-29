8c7e4579dc2e40709eef70e57d3fbb4914c4e80b
mattrick16 <matthewhighrollers@gmail.com>
2014-12-28 18:53:49 -0800

    Add ClaimedChunks. TODO Add SettlementPlayer

--- src/me/projectx/settlements/Settlements.java
+++ src/me/projectx/settlements/Settlements.java
@@ -1,5 +1,18 @@
 package me.projectx.settlements;
 
-public class Settlements {
-	
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Settlements extends JavaPlugin {
+
+	private Plugin plugin;
+
+	public void onEnable() {
+		plugin = this;
+	}
+
+	public Plugin getPlugin() {
+		return plugin;
+	}
+
 }
