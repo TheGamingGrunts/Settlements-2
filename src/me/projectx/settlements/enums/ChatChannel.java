161b8f18a1117505f6bb16468a48ade000d655a5
giantsfan <thegurugamers@gmail.com>
2014-12-29 22:20:05 -0500

    Add chat channels

---
+++ src/me/projectx/settlements/enums/ChatChannel.java
@@ -0,0 +1,11 @@
+package me.projectx.settlements.enums;
+
+/**
+ * An enum containing the various chat modes players can be in
+ * 
+ * @author Daniel
+ */
+public enum ChatChannel {
+	
+	GLOBAL, SETTLEMENT, ALLIANCE;
+}
