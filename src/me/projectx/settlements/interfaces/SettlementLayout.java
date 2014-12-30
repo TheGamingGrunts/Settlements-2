b76c64968c16cddedb9af9a1e4a302ecb6274f71
giantsfan <thegurugamers@gmail.com>
2014-12-29 22:22:18 -0500

    Add a bunch of methods.

--- src/me/projectx/settlements/interfaces/SettlementLayout.java
+++ src/me/projectx/settlements/interfaces/SettlementLayout.java
@@ -1,5 +1,11 @@
 package me.projectx.settlements.interfaces;
 
+import java.util.UUID;
+
+import me.projectx.settlements.models.SettlementPlayer;
+
+import org.bukkit.Location;
+
 public interface SettlementLayout {
 	
 	/**
@@ -44,4 +50,138 @@ public interface SettlementLayout {
 	 */
 	public double getBalance();
 	
+	/**
+	 * Set a short description of the Settlement
+	 * 
+	 * @param tagline : The description
+	 */
+	public void setTagline(String tagline);
+	
+	/**
+	 * Get the tagline of the Settlement
+	 * 
+	 * @return The Settlement's tagline
+	 */
+	public String getTagline();
+	
+	/**
+	 * Give a player citizenship in the Settlement
+	 * 
+	 * @param player : The UUID of the player
+	 */
+	public void giveCitizenship(SettlementPlayer player);
+	
+	/**
+	 * Revoke a player's citizenship (officer & citizens)
+	 * 
+	 * @param player : The UUID of the player
+	 */
+	public void revokeCitizenship(SettlementPlayer player);
+	
+	/**
+	 * Promote a player in the Settlement
+	 * 
+	 * @param player : The player to promote
+	 */
+	public void promote(SettlementPlayer player);
+	
+	/**
+	 * Get the leader of the Settlement
+	 * 
+	 * @return The leader of the Settlement
+	 */
+	public SettlementPlayer getLeader();
+	
+	/**
+	 * Determine if the Settlement has a player
+	 * 
+	 * @param player : The UUID of the player
+	 * @return True if the Settlement has the player, false if not.
+	 */
+	public boolean hasMember(SettlementPlayer player);
+	
+	/**
+	 * Get the total number of members in the Settlement
+	 * 
+	 * @return The number of members in the Settlement
+	 */
+	public int memberSize();
+	
+	/**
+	 * Send a message to all Settlement members
+	 * 
+	 * @param message : The message to send
+	 */
+	public void sendSettlementMessage(String message);
+	
+	/**
+	 * Send a message to all members of allied Settlements
+	 * 
+	 * @param message : The message to send
+	 */
+	public void sendAllianceMessage(String message);
+	
+	/**
+	 * Determine if a Settlement is in this Settlement's alliance
+	 * 
+	 * @param id : The ID of the Settlement to test
+	 * @return True if the Settlement is an alliance member, false if not
+	 */
+	public boolean hasAlly(long id);
+	
+	/**
+	 * Add a Settlement to this Settlement's alliance
+	 * 
+	 * @param id : The ID of the Settlement to add
+	 */
+	public void addAlly(long id);
+	
+	/**
+	 * Remove a Settlement from this Settlement's alliance
+	 * 
+	 * @param id : The ID of the Settlement to remove
+	 */
+	public void removeAlly(long id);
+	
+	/**
+	 * Get the Settlement's home
+	 * 
+	 * @return The location of the Settlement's home
+	 */
+	public Location getHome();
+	
+	/**
+	 * Set the location of the Settlement's home
+	 * 
+	 * @param location : The location
+	 */
+	public void setHome(Location location);
+	
+	/**
+	 * Determine if the Settlement has a home set
+	 * 
+	 * @return True if the home is set, false if not.
+	 */
+	public boolean hasHome();
+	
+	/**
+	 * Determine if the Settlement has any online members
+	 * 
+	 * @return True if there is an online member, false if not
+	 */
+	public boolean hasOnlineMember();
+	
+	/**
+	 * Set the leader who will take over once the current leader leaves
+	 * 
+	 * @param id : The ID of the next leader
+	 */
+	public void setQueuedLeader(UUID id);
+	
+	/**
+	 * Get the next leader of the Settlement
+	 * 
+	 * @return The next leader
+	 */
+	public UUID getQueuedLeader();
 }
--- src/me/projectx/settlements/models/Settlement.java
+++ src/me/projectx/settlements/models/Settlement.java
@@ -1,5 +1,190 @@
 package me.projectx.settlements.models;
 
-public class Settlement {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import me.projectx.settlements.enums.Rank;
+import me.projectx.settlements.interfaces.SettlementLayout;
+import me.projectx.settlements.managers.PlayerManager;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+/**
+ * The Settlement object, containing all Settlement-related methods.
+ * 
+ * @author Daniel
+ */
+public class Settlement implements SettlementLayout {
 	
+	private long id;
+	private double balance;
+	private String name, tagline;
+	private UUID leader, queuedLeader; //Will update this to SettlementPlayer later
+	private Location home;
+	private List<Long> allies = new ArrayList<Long>();
+	private List<SettlementPlayer> members = new ArrayList<SettlementPlayer>();
+	
+	public Settlement(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	@Override
+	public long getId() {
+		return id;
+	}
+	
+	@Override
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	@Override
+	public double getBalance() {
+		return balance;
+	}
+	
+	@Override
+	public void setTagline(String tagline) {
+		this.tagline = tagline;
+		
+	}
+	
+	@Override
+	public String getTagline() {
+		return tagline;
+	}
+	
+	@Override
+	public void giveCitizenship(SettlementPlayer player) {
+		members.add(player);
+		player.setRank(Rank.CITIZEN);
+		player.setSettlement(this);
+		/**
+		 * TODO
+		 * PlayerManager#updatePlayer(player) 
+		 * 
+		 * This will make a DB call that refreshes the player's settlement and rank
+		 */
+	} 
+	
+	@Override
+	public void revokeCitizenship(SettlementPlayer player) {
+		members.remove(player);
+	}
+	@Override
+	public boolean hasMember(SettlementPlayer player) {
+		return members.contains(player);
+	}
+	
+	@Override
+	public int memberSize() {
+		return members.size();
+	}
+	
+	@Override
+	public void sendSettlementMessage(String message) {
+		for (Player p : Bukkit.getOnlinePlayers()){
+			for (SettlementPlayer sp : members){
+				if (sp.getUniqueId().equals(p.getUniqueId())){
+					p.sendMessage(message);
+				}
+			}
+		}
+	}
+	
+	@Override
+	public void sendAllianceMessage(String message) {
+		//for (Long l : allies){
+			//TODO must create manager first
+		//}
+	}
+	
+	@Override
+	public boolean hasAlly(long id) {
+		return allies.contains(id);
+	}
+	
+	@Override
+	public void addAlly(long id) {
+		allies.add(id);
+	}
+	
+	@Override
+	public void removeAlly(long id) {
+		allies.remove(id);
+	}
+	
+	@Override
+	public Location getHome() {
+		return null;
+	}
+	
+	@Override
+	public void setHome(Location location) {
+		this.home = location;
+	}
+	
+	@Override
+	public boolean hasHome() {
+		return home != null;
+	}
+	
+	@Override
+	public boolean hasOnlineMember() {
+		for (Player p : Bukkit.getOnlinePlayers()){
+			for (SettlementPlayer sp : members){
+				if (sp.getUniqueId().equals(p.getUniqueId())){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	@Override
+	public void setQueuedLeader(UUID id) { //Will update this to SettlementPlayer later
+		this.queuedLeader = id;
+	}
+	
+	@Override
+	public UUID getQueuedLeader() { //Will update this to SettlementPlayer later
+		return queuedLeader;
+	}
+
+	@Override
+	public void promote(SettlementPlayer player) {
+		SettlementPlayer sp = PlayerManager.getManager().getPlayer(player);
+		if (sp.getRank() == Rank.CITIZEN){
+			sp.setRank(Rank.OFFICER);
+			//PlayerManager#updatePlayer();
+		}
+	}
+
+	@Override
+	public SettlementPlayer getLeader() {
+		for (SettlementPlayer sp : members){
+			if (sp.getRank() == Rank.LEADER){
+				return sp;
+			}
+		}
+		return null;
+	}
 }
